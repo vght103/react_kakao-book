a3f122480f25d95bc1d52c5fc330311b56145d14
vght103 <74849404+vght103@users.noreply.github.com>
2021-08-12 01:09:22 +0900

    img url modified

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 
 const Header = (props) => (
   <div className={styles.header}>
-    <img src="/imgs/logo.png" alt="logo" className={styles.logo_img} />
+    <img src="./imgs/logo.png" alt="logo" className={styles.logo_img} />
     <h1 className={styles.head_title}>Kakao Book</h1>
 
     <input type="text" className={styles.search_bar} />
